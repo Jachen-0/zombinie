73a2795cabe267c6ea7e53a375eb6417517a3854
Jachen Liu <liuj6@wit.edu>
2018-04-10 15:49:37 -0400

    added basic zombie class

---
+++ src/application/Zombie.java
@@ -0,0 +1,21 @@
+package application;
+
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Shape;
+import javafx.event.EventHandler;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+public class Zombie extends WorldObject{
+	public OrderedPair worldPos;
+	static Circle hitBox = new Circle();
+	public Zombie(double worldXPos, double worldYPos, double radius) {
+		super(hitBox, new OrderedPair(worldXPos, worldYPos), new OrderedPair(radius, radius), true);
+		
+		hitBox.setCenterX(worldXPos);
+		hitBox.setCenterY(worldYPos);
+		hitBox.setRadius(radius);
+		hitBox.setStroke(Color.GREEN);
+		hitBox.setFill(Color.RED);
+	}
+}
