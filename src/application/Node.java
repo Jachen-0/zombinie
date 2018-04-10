222e65dbf9712236b3d2d4c1677394d70b623e8b
omarnassif98 <33793027+omarnassif98@users.noreply.github.com>
2018-04-10 15:11:44 -0400

    Nodes are invisible

--- src/application/Node.java
+++ src/application/Node.java
@@ -7,6 +7,6 @@ public class Node extends WorldObject{
 	public OrderedPair worldPos;
 	
 	public Node(double worldXPos, double worldYPos, double radius) {
-		super(new Circle(), new OrderedPair(worldXPos, worldYPos), new OrderedPair(radius, radius), false);
+		super(null, new OrderedPair(worldXPos, worldYPos), new OrderedPair(radius, radius), false);
 	}
 }
